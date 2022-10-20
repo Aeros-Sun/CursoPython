d463d2e25dd4e6460eb83c70e3fcdeab10593705
Ronal Esquivel <ronalgold@gmail.com>
2022-10-20 17:04:09 -0400

    Clase practica encriptar y desencriptar archivos

--- Main.py
+++ Main.py
@@ -1,12 +1,60 @@
 
-seguirChateando = True
-while seguirChateando:
-    texto = input(">")
-    if texto == "salir":
-        seguirChateando = False
-    texto = texto.replace( ":)", " 😊 ")
-    texto = texto.replace( ":*", " 😘 ")
-    texto = texto.replace( ":p", " 😏 ")
-    print(texto)
-    
+
+from tkinter import E
+
+
+def encriptar(texto):
+    print("El texto a encriptar es: " + texto)
+
+    textoFinal = ""
+    for letra in texto:
+        textoFinal+= letra + "x"
+    return textoFinal
+
+
+def desencriptar(texto):
+    print("El texto a desencriptar es: " + texto)
+
+    textoFinal = ""
+    contador = 0
+    for letra in texto:
+        if contador %2 == 0:
+            textoFinal+= letra 
+        contador += 1
+    return textoFinal
+
+
+def encriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo, "r")
+    texto = archivo.read()
+    archivo.close()
+    textoEncriptado = encriptar(texto)
+
+    archivo = open(rutaArchivo, "w")
+    archivo.write(textoEncriptado)
+    archivo.close()
+
+    print("El texto se encripto correctamente")
+
+
+def desencriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo, "r")
+    texto = archivo.read()
+    archivo.close()
+    textoDesencriptado = desencriptar(texto)
+
+    archivo = open(rutaArchivo, "w")
+    archivo.write(textoDesencriptado)
+    archivo.close()   
+
+    print("El texto se desencripto correctamente")
+
+respuestaEoD = input(" Presione E para encriptar, o D para desencriptar")
+rutaArchivo= input("ingrese la ruta del archivo")
+
+if respuestaEoD == "E": 
+    encriptarArchivo(rutaArchivo)
+else:
+   desencriptarArchivo(rutaArchivo)
  
+
